488de211915dcafbf0da010d77ec6960bc563565
Ngseri <ngaothong.seri@gmail.com>
2015-03-24 16:16:58 -0400

    Usecase3
    usecase3_upload
    usecase3_comment

---
+++ usecase3.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Usecase3</title>
+</head>
+<bod>
+  <php?
+    $filename = $_POST('filename');
+	$tag = $_POST('comic_tag');
+	$description = $_POST('description');
+	echo "$filename is uploaded successfully";
+  ?>
+</body>
+
+</html>
