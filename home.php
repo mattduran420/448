c86d1d61cf5398fd5169e45a1f3125ae08a656da
matt <matthew.d.duran@gmail.com>
2015-04-18 19:30:38 -0400

    added check

--- home.php
+++ home.php
@@ -1,4 +1,9 @@
 <?php
+//check if user is logged in
+if($_SESSION['is_aunthenticated']){
+	header('Location: /login.php');
+}
+
 include('header.php');
 //include('sidebar.php');
 include('footer.php');
