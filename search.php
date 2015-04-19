2f12d9bb35405ccf832c834112cb2326af1f1538
deekirsh <dak@Daniels-MacBook-Pro.local>
2015-04-19 12:47:45 -0400

    hi

--- search.php
+++ search.php
@@ -6,17 +6,11 @@ include('header.php');
     
     <p>
 	
-	<form name ="searchform" action = "success.html">
-	
-		<label>
-		  	Enter search keywords: <input type = "text" name = "search" size = "30" maxlength="30" />
-		</label>
-		
-		<br />
-		<br />
+	<form name ="search" action = "search_results.php">
 
 		<label>
-		  	Search by tag: <select name="tags">
+		  	Tag: <select name="tags">
+		  	<option disabled selected></option>
 			<option>Music</option>
 			<option>Games</option>
 			<option>Movies</option>
@@ -38,6 +32,7 @@ include('header.php');
 		
 		<label>
 		  	Date: Month <select name="month">
+		  	<option disabled selected></option>
 		  	<option>January</option>
 		  	<option>February</option>
 		  	<option>March</option>
@@ -52,60 +47,10 @@ include('header.php');
 		  	<option>December</option>
 			</select>
 		</label>
-		
-		<label>
-		  	Day <select name="day">
-		  	<option>1</option>
-		  	<option>2</option>
-		  	<option>3</option>
-		  	<option>4</option>
-		  	<option>5</option>
-		  	<option>6</option>
-		  	<option>7</option>
-		  	<option>8</option>
-		  	<option>9</option>
-		  	<option>10</option>
-		  	<option>11</option>
-		  	<option>12</option>
-		  	<option>13</option>
-		  	<option>14</option>
-		  	<option>15</option>
-		  	<option>16</option>
-		  	<option>17</option>
-		  	<option>18</option>
-		  	<option>19</option>
-		  	<option>20</option>
-		  	<option>21</option>
-		  	<option>22</option>
-		  	<option>23</option>
-		  	<option>24</option>
-		  	<option>25</option>
-		  	<option>26</option>
-		  	<option>27</option>
-		  	<option>28</option>
-		  	<option>29</option>
-		  	<option>30</option>
-		  	<option>31</option>
-			</select>
-		</label>
 
 		<label>
 			Year <select name="year">
-			<option>1990</option>
-			<option>1991</option>
-			<option>1992</option>
-			<option>1993</option>
-			<option>1994</option>
-			<option>1995</option>
-			<option>1996</option>
-			<option>1997</option>
-			<option>1998</option>
-			<option>1999</option>
-			<option>2000</option>
-			<option>2001</option>
-			<option>2002</option>
-			<option>2003</option>
-			<option>2004</option>
+			<option disabled selected></option>
 			<option>2005</option>
 			<option>2006</option>
 			<option>2007</opion>
@@ -123,7 +68,15 @@ include('header.php');
 		<br />
 		<br />
 
-		<input type = "submit" value="Submit" />
+		<label>
+		  	Keyword: <input type = "text" name = "search" size = "30" maxlength="30" />
+		</label>
+		
+		<br />
+		<br />
+
+		<input type = "submit" value="Submit"/>
+		<input type = "reset" value="Reset"/>
 
 	</form>
 
