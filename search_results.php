c7cbe0049ec8c0f5fa2688181017e66aebb37fae
deekirsh <dak@Daniels-MacBook-Pro.local>
2015-04-19 16:35:10 -0400

    m

--- search_results.php
+++ search_results.php
@@ -63,7 +63,7 @@ while($row = mysql_fetch_array($result)){
    	print ("<br />");
    	$comic_file = $row['img_url'];
    	$comic_file_path = "/assets/uploads/" . $comic_file;
-   	?> <img src=<?php $comic_file_path ?>>
+   	?> <img src = <?php $comic_file_path ?>/>
    	<?php
    	print("<br />");
 }
