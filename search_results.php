48c171642d10437f6b34cc6178fbc069dc432843
deekirsh <dak@Daniels-MacBook-Pro.local>
2015-04-19 16:16:28 -0400

    testing output

--- search_results.php
+++ search_results.php
@@ -61,7 +61,8 @@ $result = mysql_query($select_query);
 while($row = mysql_fetch_array($result)){
    	print ("$row[comic_name]");
    	print ("<br />");
-   	print ("$row[img_url]");
+   	$comic_file = $row['img_url']);
+   	echo $comic_file;
    	print("<br />");
 }
 
