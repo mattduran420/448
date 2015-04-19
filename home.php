008f6cb6f890ef4f33e721b849dc10b5c39f8b83
matt <matthew.d.duran@gmail.com>
2015-04-18 20:45:57 -0400

    fix auth example

--- home.php
+++ home.php
@@ -1,8 +1,8 @@
 <?php
 //check if user is logged in
-if(!$_SESSION['is_authenticated']){
-	header('Location: /login.php');
-}
+//if(!$_SESSION['is_authenticated']){
+//	header('Location: /login.php');
+//}
 
 include('header.php');
 //include('sidebar.php');
