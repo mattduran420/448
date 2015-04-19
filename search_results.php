c0052d566e3963c47255bf06f3c71873bfb14e64
deekirsh <dak@Daniels-MacBook-Pro.local>
2015-04-19 16:37:21 -0400

    cheeseburger

--- search_results.php
+++ search_results.php
@@ -63,7 +63,7 @@ while($row = mysql_fetch_array($result)){
    	print ("<br />");
    	$comic_file = $row['img_url'];
    	$comic_file_path = "/assets/uploads/" . $comic_file;
-   	?> <img src = <?php $comic_file_path ?>/>
+   	?> <img src ="/assets/uploads/flarp.jpg"/>
    	<?php
    	print("<br />");
 }
