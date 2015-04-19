7a93b14ca0962fbe1d0580e2d76aa5c6b5dea053
matt <matthew.d.duran@gmail.com>
2015-04-19 14:28:45 -0400

    readjusted collaborate

--- collaborate.php
+++ collaborate.php
@@ -1,45 +1,44 @@
 <?php include('header.php'); ?>
 
-				<!-- EDIT HERE -->
-				<div align="center">
-					<h3>Use Case 2</h3>
-					<h2>Drafts</h2>
-					<div id="comic-playground">
-						<!-- BEGIN SAMPLE COMIC NOTES -->
-						<!-- COMIC NOTES WILL BE INSERTED DYNAMICALLY -->
-						<div id="note-container">
-							<!-- FOR NOTE IN NOTES -->
-							<div class="comic-note" style="margin-top:88px;margin-left:55px;">
-								<a class="note-link" href="javascript:void(0)" onclick="toggleNote('note-1');">X</a>
-								<div class="note-body" id="note-1">
-									<p>Note added by <a href="#">Jake</a><br/>
-									1:58 AM 3/17/2015
-									</p>
-									<p>I have no friends!</p>
-								</div>
-							</div>
-							<!-- END FOOR LOOP -->
-						<!-- END SAMPLE COMIC NOTES -->
-						<!-- img src $path + $imageName class="comic-spotlight"> -->
-						<img id="comic" src="assets/img/sample_comic.png" class="comic-spotlight">
-					</div>
-
+<!-- EDIT HERE -->
+<div align="center">
+	<h3>Use Case 2</h3>
+	<h2>Drafts</h2>
+	<div id="comic-playground">
+		<!-- BEGIN SAMPLE COMIC NOTES -->
+		<!-- COMIC NOTES WILL BE INSERTED DYNAMICALLY -->
+		<div id="note-container">
+			<!-- FOR NOTE IN NOTES -->
+			<div class="comic-note" style="margin-top:88px;margin-left:55px;">
+				<a class="note-link" href="javascript:void(0)" onclick="toggleNote('note-1');">X</a>
+				<div class="note-body" id="note-1">
+					<p>Note added by <a href="#">Jake</a><br/>
+					1:58 AM 3/17/2015
+					</p>
+					<p>I have no friends!</p>
 				</div>
+			</div>
+			<!-- END FOOR LOOP -->
+			<!-- END SAMPLE COMIC NOTES -->
+			<!-- img src $path + $imageName class="comic-spotlight"> -->
+			<img id="comic" src="assets/img/sample_comic.png" class="comic-spotlight">
+		</div>
+	</div>
+</div>
 
-				<!-- BEGIN LISTED NOTES -->
-				<div class="something">
-					<!-- FOR NOTE IN NOTES -->
-					<div class="listed-comic-note">
-						<div class="comment-body">
-							<p>Note added by <a href="#">Jake</a><br/>
-							1:58 AM 3/17/2015
-							</p>
-							<p>I have no friends!</p>
-						</div>
-					</div>
-					<!-- END FOOR LOOP -->
-				</div>
-				<!-- END LISTED NOTES -->
-
+<!-- BEGIN LISTED NOTES -->
+<div class="something">
+	<!-- FOR NOTE IN NOTES -->
+	<div class="listed-comic-note">
+		<div class="comment-body">
+			<p>Note added by <a href="#">Jake</a><br/>
+			1:58 AM 3/17/2015
+			</p>
+			<p>I have no friends!</p>
+		</div>
+	</div>
+	<!-- END FOOR LOOP -->
+</div>
+<!-- END LISTED NOTES -->
 
 <?php include('footer.php'); ?>
