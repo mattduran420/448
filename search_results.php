1fca14ab50f63ef33c2280a0ac272bf1ae458d66
deekirsh <dak@Daniels-MBP.home>
2015-04-19 22:09:16 -0400

    more fixes

--- search_results.php
+++ search_results.php
@@ -59,17 +59,22 @@ $select_query = "SELECT comic_name, comic_id FROM db_comic WHERE (genre='$genre'
 				OR MONTH(upload_date)='$month' OR YEAR(upload_date)='$year' OR tag='$tag'";
 	
 $result = mysql_query($select_query);
+
 if(!mysql_query($select_query,$db)){
-			 die('Insert Row Failed!' . mysql_error());
-			}
+	die('Insert Row Failed!' . mysql_error());
+}
 
 while($row = mysql_fetch_array($result)){
-   	print ("$row[comic_name]");
-   	print ("<br />");
-   	$comic_id = $row['comic_id'];
-   	?><img src =images.php?id=<?php echo $comic_id ?>>
-   	<?php
-   	print("<br />");
+	if(empty($row)){
+		print"No results found."
+	}else{
+	   	print ("$row[comic_name]");
+	   	print ("<br />");
+	   	$comic_id = $row['comic_id'];
+	   	?><img src =images.php?id=<?php echo $comic_id ?>>
+	   	<?php
+	   	print("<br />");
+	}
 }
 
 include('footer.php');
