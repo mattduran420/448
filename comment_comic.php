f73310c9a2cc4221ea90b1713dba4cd050c879f6
Ngseri <ngaothong.seri@gmail.com>
2015-04-19 22:55:10 -0400

    Unbelievable!!! We did it!!!

--- comment_comic.php
+++ comment_comic.php
@@ -1,10 +1,10 @@
-<!--Seri Ngaothong -->
-	<?php
+<?php
+	//Seri Ngaothong
 		include('header.php');
 	
 
 		$comic = $_GET['comic_id'];
-		
+	
 
 		//Connect to the database
 		$comic_table = mysql_connect("studentdb.gl.umbc.edu", "mduran2", "mduran2");
@@ -19,18 +19,18 @@
 		//Upload data into the database
 		$comic_data = mysql_query("SELECT (comic_name, genre, tag, upload_date, user_id, comic_status, rating_total, rating_count) FROM db_comic WHERE comic_id = $comic");
 		//Display comment on the page
-		$getquery = mysql_query("SELECT * FROM db_comment ORDER BY user_id DESC");
+		/*$getquery = mysql_query("SELECT * FROM db_comment ORDER BY user_id DESC");
 			while($rows = mysql_fetch_array($getquery)){
 				$user_id = $rows['user_id'];
-				$comic = $rows['comic'];
+				//$comic = $rows['comic'];
 				$comment = $rows['comment'];
 
 				echo $user_id . '<br />' . $comment;
-			}
+			}*/
 		?>
 
 
-		<img src = "http://userpages.umbc.edu/~mduran2/is448/project/images.php?id=<?php echo $comic; ?>">
+		<img src = "http://userpages.umbc.edu/~mduran2/is448/project/images.php?id=<?php echo $comic; ?>"><br />
 		<form action = "comment_process.php" method = "post">
 			<textarea name = "comment_area">
 				
@@ -42,16 +42,19 @@
 
 		<?php
 
-		$get_comment = "SELECT * FROM db_comment WHERE comic_id = $comic";
-		$query = mysql_query($get_comment);
+		$get_comment = "SELECT * from db_comment where comic_id = '". $comic . "'";
+		$query = mysql_query($get_comment, $comic_table);
 
+		if(!$query){
+			 die('Insert Row Failed!' . mysql_error());
+		}
 			while($rows = mysql_fetch_array($query)){
 				$user_id = $rows['user_id'];
-				$comic = $rows['comic_id'];
+				//$comic = $rows['comic_id'];
 				$comment = $rows['comment_body'];
 				$timestamp = $row['timestamp'];
 
-				echo $user_id . '<br />' . $comic . '<br />' . $comment . '<br />'.$timestamp;
+				echo $comment . " : from " . $user_id . '<br />';
 			}
 
 
