2e647ce71cdb1ff37fae1603a24ba6dc4425fa30
matt <matthew.d.duran@gmail.com>
2015-04-19 23:19:32 -0400

    functional view_drafts.php
    basic, needs visual updates.

--- view_drafts.php
+++ view_drafts.php
@@ -1,9 +1,28 @@
 <?php
-include("header.php")
-?>
+include("header.php");
+$db = mysql_connect("studentdb.gl.umbc.edu","mduran2","mduran2");
+if(!$db) exit("Error - could not connect to MySQL");
 
+$er = mysql_select_db("mduran2");
+if(!$er) exit("Error - could not select db_user database");
 
+$querystring  = "SELECT comic_id,comic_name, genre, tag, upload_date FROM db_comic
+	 where user_id = 1 and comic_status = 1";
+$results = mysql_query($querystring, $db);
+if(!$results){
+	die("error:" . mysql_error());
+}
 
-<?php
+while($row = mysql_fetch_array($results)){
+	echo '<a href=edit_draft.php?id='.$row['comic_id'].'>';
+	echo '<img src="images.php?id='.$row['comic_id'].'">';
+	echo '</a><br/>';
+	echo $row['comic_name'] . " ";
+	echo $row['genre'] . " ";
+	echo $row['tag'] . " ";
+	echo $row['upload_date'] . " ";
+	echo "<br/>" . " ";
+}
+
 include("footer.php");
 ?>
