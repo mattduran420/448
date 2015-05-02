c05aaecfcfeac0c9d960be16d98446c465f54161
matt <matthew.d.duran@gmail.com>
2015-05-02 13:08:50 -0400

    fixed JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
+//debug line
+console.log("main.js included");
+
 function checkSearch(){
+	console.log("checkSearch invoked");
 	var genre = document.getElementByID("genre").value;
 	var month = document.getElementByID("month").value;
 	var year = document.getElementByID("year").value;
 	var tag = document.getElementByID("tag").value;
 
-	if(genre="" || month="" || year="" || tag=""){
+	if(genre=="" || month=="" || year=="" || tag==""){
 		alert("No search criteria entered. Please enter one or more search criteria!");
 		return false;
 	}else{
