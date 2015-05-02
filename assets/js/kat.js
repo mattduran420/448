e7406ed65caf51521da30ed4b8b3d9c1bda99480
Kat Pfeiffer <katp1@umbc.edu>
2015-05-02 14:05:37 -0400

    stuff

--- assets/js/kat.js
+++ assets/js/kat.js
@@ -1,3 +1,6 @@
+//debug line
+console.log("kat.js included");
+
 function checkRegistration() {
   var upass = document.getElementById("upass").value;
 
@@ -15,7 +18,7 @@ function checkRegistration() {
 	
   var email = document.getElementById("email").value;
 
-	var pattern2 = ^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$;
+	var pattern2 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 	var result2 = pattern2.test(email);
 	
 	if (result2==false) 
@@ -25,4 +28,5 @@ function checkRegistration() {
 	document.getElementById("email").select();
     
 		return false;
+	}
 	}
