734f0fc2343b608474eb7e467d76fee1962d2678
deekirsh <dak@Daniels-MBP.home>
2015-05-06 23:02:40 -0400

    finguhz crozzed

--- create_account.php
+++ create_account.php
@@ -8,15 +8,13 @@
 	$user_password = $_POST['user_password'];
 	$email = $_POST['email'];
 	$db = mysql_connect("studentdb.gl.umbc.edu","mduran2","mduran2");
-	?>
-	<script type="text/javascript"> 
-	function checkRegistration() {
-  	var upass = document.getElementById("upass").value;
-
-	var pattern1 = /[0-9a-zA-Z]{6,}/;
-	var result = pattern1.test(upass);
 	
-	if (result==false) 
+  	var $upass = document.getElementById("upass").value;
+
+	var $pattern1 = /[0-9a-zA-Z]{6,}/;
+	var $result = $pattern1.test($upass);
+	
+	if ($result==false) 
 	{
 
 	alert("The password you entered is not in the correct form. Must be at least 6 characters long.");
@@ -25,12 +23,12 @@
 		return false;
 	}
 	
-  	var email = document.getElementById("email").value;
+  	var $email = document.getElementById("email").value;
 
-	var pattern2 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-	var result2 = pattern2.test(email);
+	var $pattern2 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	var $result2 = $pattern2.test($email);
 	
-	if (result2==false) 
+	if ($result2==false) 
 	{
 
 		alert("The email address you entered is not in the correct form.");
@@ -39,12 +37,12 @@
 		return false;
 	}
 	
-	var uname = document.getElementById("uname").value;
+	var $uname = document.getElementById("uname").value;
 
-	var pattern3 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-	var result3 = pattern3.test(uname);
+	var $pattern3 = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	var $result3 = $pattern3.test($uname);
 	
-	if (result3==false) 
+	if ($result3==false) 
 	{
 
 		alert("This username is already taken. Please choose a different one.");
@@ -52,9 +50,7 @@
     
 		return false;
 	}
-	}
-	</script>
-	<?php
+
 	if(!$db)
 	{
 		exit("Error - could not connect to MySQL");
